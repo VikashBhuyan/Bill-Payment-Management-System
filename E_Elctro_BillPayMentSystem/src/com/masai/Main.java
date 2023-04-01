c099d7cd4847c566643ce9c38d008b294c04b09b
VikashBhuyan <vikashbhuyan2000@gmail.com>
2023-04-01 22:05:59 +0530

    added

--- E_Elctro_BillPayMentSystem/src/com/masai/Main.java
+++ E_Elctro_BillPayMentSystem/src/com/masai/Main.java
@@ -6,7 +6,11 @@ import java.util.Scanner;
 import com.masai.utility.Admin;
 import com.masai.utility.FileExist;
 import com.masai.entities.Consumer;
+import com.masai.exceptions.BillingException;
 import com.masai.exceptions.InvalidDetailsException;
+import com.masai.exceptions.ProductException;
+import com.masai.services.ConsumerService;
+import com.masai.services.ConsumerServiceImp;
 
 import java.io.*;
 import java.time.LocalDate;
@@ -16,14 +20,15 @@ public class Main {
 
 	public static void adminFunctionality(Scanner sc, Map<String, Consumer> consumer, List<Billing> bill) throws InvalidDetailsException
 	{
+		ConsumerService cus = new ConsumerServiceImp();
 		adminLogin(sc);
 		int choice = 0;
 		try {
 			do {
 				System.out.println("Press 1 Create Billing Cycle for Consumer");
-				System.out.println("Press 2 Get Consumer");
+				System.out.println("Press 2 Get Customer Detail");
 				System.out.println("Press 3 Get All Consumer");
-				System.out.println("Press 4 Delete Consumer");
+				System.out.println("Press 4 Delete A Consumer");
 				System.out.println("Press 5 Log Out");
 				choice = sc.nextInt();
 				switch (choice) {
@@ -31,19 +36,16 @@ public class Main {
 					createBillForConsumer(consumer,bill,sc);
 					break;
 				case 2:
-					
+					getCustomerDetail(sc,cus,consumer);
 					break;
 				case 3:
-
+					getAllCustomerDetails(cus,consumer);
 					break;
 				case 4:
-
+					deleteACustomer(cus,sc,consumer);
 					break;
 				case 5:
-
-					break;
-				case 6:
-
+					System.out.println("admin has successfully logout");
 					break;
 				default:
 					throw new IllegalArgumentException("Unexpected value: " + choice);
@@ -53,6 +55,12 @@ public class Main {
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
+	}
+	
+	public static void deleteACustomer(ConsumerService cus,Scanner sc,Map<String,Consumer> consumer) throws ProductException {
+		System.out.println("Enter the username of cus to be deleted");
+		String username = sc.next();
+		cus.deleteCustomer(username,consumer);
 	}
 	
 	public static void adminLogin(Scanner sc) throws InvalidDetailsException {
@@ -66,6 +74,28 @@ public class Main {
 			System.out.println("successfully login");
 		} else {
 			throw new InvalidDetailsException("Invalid Admin Credentials");
+		}
+	}
+	
+	public static void getCustomerDetail(Scanner sc,ConsumerService cus, Map<String, Consumer> consumer) {
+		System.out.println("Enter Consumer email");
+		String username = sc.next();
+		Consumer oneCus = cus.viewCustomerDetails(username,consumer);
+		System.out.println("Firstname : " + oneCus.getFirstName());
+		System.out.println("Lastname : " + oneCus.getLastName());
+		System.out.println("address : " + oneCus.getAddress());
+		System.out.println("email : " + oneCus.getEmail());
+		System.out.println("username : " + oneCus.getUsername());
+		System.out.println("password : " + oneCus.getPassword());
+		System.out.println("phone : " + oneCus.getPhoneNo());
+		System.out.println("wallet balance : " + oneCus.getBalance());
+	}
+	
+	public static void getAllCustomerDetails(ConsumerService cus,Map<String,Consumer> consumer) throws ProductException{
+		List<Consumer> li = cus.viewAllCustomers(consumer);
+		for(Consumer x : li)
+		{
+			System.out.println(x);
 		}
 	}
 	
@@ -92,6 +122,23 @@ public class Main {
 	    }
 	}
 	
+	public static void consumerFunctionality(Scanner sc, Map<String,Consumer> consumer, List<Billing> bill) throws InvalidDetailsException, BillingException{
+		ConsumerService cus = new ConsumerServiceImp();
+		
+		System.out.println("please enter the following details to login");
+		System.out.println("please enter the username provided by system");
+		String username = sc.next();
+		System.out.println("Enter the password");
+		String pass = sc.next();
+		consumerLogin(username ,pass, consumer, cus);
+	}
+	
+	public static void consumerLogin(String username,String pass, Map<String, Consumer> customers, ConsumerService cusService)
+			throws InvalidDetailsException {
+		cusService.login(username, pass,customers);
+		System.out.println("Customer has successfully login");
+
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -114,7 +161,7 @@ public class Main {
 					adminFunctionality(sc,consumer,bill);
 					break;
 				case 2:
-					System.out.println("You have chosen Consumer Login");
+					consumerFunctionality(sc,consumer,bill);
 					break;
 
 				case 3:
